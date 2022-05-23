0cf0065afa31e0958020d487701deff67cf532ca
Randell Janus Nepomuceno <raaanjnpmcn@gmail.com>
2022-05-23 15:31:42 +0800

    add reset button icon

--- src/App.tsx
+++ src/App.tsx
@@ -119,8 +119,22 @@ const App: FC = () => {
           </div>
           <button
             onClick={handleRestart}
-            className="font-bold bg-violet-400 px-4 py-3 rounded-md active:scale-110 hover:bg-violet-500 transition-all text-sm sm:text-xl"
+            className="flex items-center space-x-2 font-bold bg-violet-400 px-4 py-3 rounded-md active:scale-110 hover:bg-violet-500 transition-all text-sm sm:text-xl"
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 sm:h-5 sm:w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+              />
+            </svg>
             <p>RESET</p>
           </button>
         </div>
