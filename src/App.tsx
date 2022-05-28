fce6f663a203ecd7b8982e45d6ccc9e055d3fe13
Randell Janus Nepomuceno <raaanjnpmcn@gmail.com>
2022-05-28 20:20:39 +0800

    add singleplayer mode with computer making random turns

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ResetIcon, SinglePlayer, Multiplayer } from "./components/Icons";
 import Square from "./components/Square";
 
 const App = () => {
   const [squares, setSquares] = useState<string[] | []>(Array(9).fill(null));
+  const [isSinglePlayer, setIsSinglePlayer] = useState<boolean>(true);
+
   const [isXTurn, setIsXTurn] = useState<boolean>(true);
   const [animation, setAnimation] = useState<number>(0);
 
-  const checkWinner = (squares: [] | string[]): any => {
-    const winConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
+  const isPlayerTurn =
+    squares.filter((square) => square !== null).length % 2 === 0;
+  const isComputerTurn =
+    squares.filter((square) => square !== null).length % 2 === 1;
+  const emptyIndexes = squares
+    .map((square, index) => (square === null ? index : null))
+    .filter((val) => val !== null);
+  const isBoardFull = squares.every((i) => i !== null);
+
+  const checkWinner = (squares: [] | string[]): any => {
     for (let i = 0; i < winConditions.length; i++) {
       const [a, b, c] = winConditions[i];
       if (
@@ -37,18 +49,30 @@ const App = () => {
       return;
     }
 
-    squares[i] = isXTurn ? "X" : "O";
-    setSquares(squares);
-    setIsXTurn(!isXTurn);
+    if (!isSinglePlayer) {
+      squares[i] = isXTurn ? "X" : "O";
+      setSquares(squares);
+      setIsXTurn(!isXTurn);
+
+      return;
+    }
+
+    if (isPlayerTurn) {
+      let newSquares = squares;
+      newSquares[i] = "X";
+      setSquares([...newSquares]);
+    }
   };
 
   const winningPattern = checkWinner(squares);
-  const isBoardFull = squares.every((i) => i !== null);
 
   let status;
 
   if (winningPattern) {
-    status = `Winner: ${isXTurn ? "O" : "X"}`;
+    if (!isSinglePlayer) {
+      status = `Winner: ${isXTurn ? "O" : "X"}`;
+    }
+    status = `Winner: ${isPlayerTurn ? "O" : "X"}`;
   } else if (isBoardFull && !winningPattern) {
     status = "Draw!";
   } else {
@@ -68,11 +92,43 @@ const App = () => {
     }
   };
 
+  const handleGameMode = (): void => {
+    handleRestart();
+    setIsSinglePlayer((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const putComputerAt = (i: any) => {
+      let newSquares = squares;
+      newSquares[i] = "O";
+      setSquares([...newSquares]);
+    };
+
+    if (isComputerTurn) {
+      const randomIndex =
+        emptyIndexes[Math.ceil(Math.random() * emptyIndexes.length)];
+
+      if (!winningPattern && !isBoardFull) putComputerAt(randomIndex);
+    }
+  }, [squares]);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center px-4">
       <div className="space-y-4 sm:space-y-8">
         <h1 className="font-bold">TIC-TAC-TOE</h1>
-
+        <button className="mode-btn" onClick={handleGameMode}>
+          {isSinglePlayer ? (
+            <div className="flex items-center space-x-2">
+              <SinglePlayer className="h-4 w-4 sm:h-5 sm:w-5" />
+              <p>Player vs Computer</p>
+            </div>
+          ) : (
+            <div className="flex items-center space-x-2">
+              <Multiplayer className="h-4 w-4 sm:h-5 sm:w-5" />
+              <p>Player vs Player</p>
+            </div>
+          )}
+        </button>
         <div className="grid grid-cols-3 gap-2 sm:gap-4">
           {squares.map((value, i) => (
             <Square
@@ -104,20 +160,7 @@ const App = () => {
             onClick={handleRestart}
             className="flex items-center space-x-2 reset-btn"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 sm:h-5 sm:w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-              />
-            </svg>
+            <ResetIcon className="h-4 w-4 sm:h-5 sm:w-5" />
             <p>RESET</p>
           </button>
         </div>
---
+++ src/components/Icons.tsx
@@ -0,0 +1,56 @@
+export const ResetIcon = ({ className }: { className: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+      />
+    </svg>
+  );
+};
+
+export const SinglePlayer = ({ className }: { className: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+      />
+    </svg>
+  );
+};
+
+export const Multiplayer = ({ className }: { className: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+      />
+    </svg>
+  );
+};
