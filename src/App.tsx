8cd3d87902ad474b628780f9a69fee7a13719227
Randell Janus Nepomuceno <raaanjnpmcn@gmail.com>
2022-05-29 19:29:56 +0800

    add difficulty levels

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import RadioButton from "./components/RadioButton";
 const App = () => {
   const [squares, setSquares] = useState<string[] | []>(Array(9).fill(null));
   const [gameMode, setGameMode] = useState<string>("1");
+  const [difficulty, setDifficulty] = useState<string>("easy");
 
   const [isXTurn, setIsXTurn] = useState<boolean>(true);
   const [animation, setAnimation] = useState<number>(0);
@@ -92,7 +93,7 @@ const App = () => {
     if (gameMode === "2") {
       status = "Player: " + (isXTurn ? "X" : "O");
     } else if (gameMode === "1") {
-      status = `${isPlayerTurn ? "Your turn" : "Thinking..."}`;
+      status = `${isPlayerTurn ? "Your turn" : "..."}`;
     }
   }
 
@@ -126,39 +127,47 @@ const App = () => {
     setGameMode(e.target.value);
   };
 
+  const handleDifficulty = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    handleRestart();
+    setDifficulty(e.target.value);
+  };
+
+  const emptyIndexes = squares
+    .map((square, index) => (square === null ? index : null))
+    .filter((val) => val !== null);
+  const cornerIndexes = squares
+    .map((square, index) => (square === null ? index : null))
+    .filter((val) => val === 0 || val === 2 || val === 6 || val === 8);
+  const crossIndexes = squares
+    .map((square, index) => (square === null ? index : null))
+    .filter((val) => val === 1 || val === 3 || val === 5 || val === 7);
+
+  const randomCornerIndex =
+    cornerIndexes[Math.floor(Math.random() * cornerIndexes.length)];
+  const randomCrossIndex =
+    crossIndexes[Math.floor(Math.random() * crossIndexes.length)];
+  const someCornerIndexesMarked = cornerIndexes.some((i) => i !== null);
+  const fullLines = linesThatAre("X", "X", "O");
+
   useEffect(() => {
-    const emptyIndexes = squares
-      .map((square, index) => (square === null ? index : null))
-      .filter((val) => val !== null);
-    const cornerIndexes = squares
-      .map((square, index) => (square === null ? index : null))
-      .filter((val) => val === 0 || val === 2 || val === 6 || val === 8);
-    const crossIndexes = squares
-      .map((square, index) => (square === null ? index : null))
-      .filter((val) => val === 1 || val === 3 || val === 5 || val === 7);
-
     if (isComputerTurn) {
-      // Priority: computer marks middle tile if first player turn is corner
-      if (
-        squares[4] === null &&
-        !squares[1] &&
-        !squares[3] &&
-        !squares[5] &&
-        !squares[7]
-      ) {
-        putComputerAtSquare(4);
-        return;
+      // Priority: computer marks middle tile if vacant
+      if (difficulty === "hard") {
+        if (!squares[4]) {
+          putComputerAtSquare(4);
+          return;
+        }
       }
-      // Priority: computer only marks corners if first player turn was middle tile
-      const randomCornerIndex =
-        cornerIndexes[Math.floor(Math.random() * cornerIndexes.length)];
-      if (squares[4] && emptyIndexes.length === 8) {
-        if (!winningPattern && !isBoardFull)
-          putComputerAtSquare(randomCornerIndex);
-        return;
+      // computer marks corners if first player turn was middle tile, avoids y-form
+      if (difficulty === "hard") {
+        if (squares[4] && emptyIndexes.length === 8) {
+          if (!winningPattern && !isBoardFull) {
+            putComputerAtSquare(randomCornerIndex);
+            return;
+          }
+        }
       }
-
-      // Priority: win game
+      // win game
       const winningLines = linesThatAre("O", "O", null);
       if (winningLines.length > 0) {
         const winningIndex = winningLines[0].filter(
@@ -167,7 +176,7 @@ const App = () => {
         if (!winningPattern && !isBoardFull) putComputerAtSquare(winningIndex);
         return;
       }
-      // Priority: block opponent
+      // block opponent
       const linesToBlock = linesThatAre("X", "X", null);
       if (linesToBlock.length > 0) {
         const blockIndex = linesToBlock[0].filter(
@@ -176,14 +185,38 @@ const App = () => {
         if (!winningPattern && !isBoardFull) putComputerAtSquare(blockIndex);
         return;
       }
-      // Priority: if first player turn are corners and second player turn are crosses, computer marks corners
-      const someCornerIndexesMarked = cornerIndexes.some((i) => i !== null);
-      const someCrossIndexesMarked = crossIndexes.some((i) => i !== null);
-      if (someCornerIndexesMarked && someCrossIndexesMarked) {
-        if (!winningPattern && !isBoardFull)
-          putComputerAtSquare(randomCornerIndex);
-        return;
+      // Avoids s-form
+      if (difficulty === "hard") {
+        const freeLines = linesThatAre(null, null, null);
+        if (someCornerIndexesMarked) {
+          if (freeLines.length > 0) {
+            if (!winningPattern && !isBoardFull)
+              putComputerAtSquare(
+                freeLines[0].filter((index) => squares[index] === null)[1]
+              );
+            return;
+          }
+        }
       }
+      // Avoids big triangle player move
+      if (difficulty === "hard") {
+        if (fullLines.length > 0 && squares[4] === "O") {
+          if (!winningPattern && !isBoardFull) {
+            putComputerAtSquare(randomCrossIndex);
+            return;
+          }
+        }
+      }
+      // Avoids small triangle player move
+      if (difficulty === "hard") {
+        if (fullLines.length > 0) {
+          if (!winningPattern && !isBoardFull) {
+            putComputerAtSquare(randomCornerIndex);
+            return;
+          }
+        }
+      }
+
       // Priority: prioritize turns that can lead closer to winning
       const linesToContinue = linesThatAre("O", null, null);
       if (linesToContinue.length > 0) {
@@ -192,7 +225,6 @@ const App = () => {
             linesToContinue[0].filter((index) => squares[index] === null)[0]
           );
         }
-
         return;
       }
       // randomize computer turn if priority moves are not applicable
@@ -206,10 +238,39 @@ const App = () => {
     <div className="min-h-screen flex flex-col items-center justify-center px-4">
       <div className="space-y-4 sm:space-y-8">
         <h1 className="font-bold text-center">TIC-TAC-TOE</h1>
-        <div onChange={handleGameMode} className="flex justify-evenly">
-          <RadioButton label="Single-player" value={"1"} gameMode={gameMode} />
-          <RadioButton label="Two-player" value={"2"} gameMode={gameMode} />
+        <div className="space-y-2">
+          <div onChange={handleGameMode} className="flex justify-evenly">
+            <RadioButton
+              label="Single-player"
+              value={"1"}
+              defaultChecked={gameMode}
+              name="mode"
+            />
+            <RadioButton
+              label="Two-player"
+              value={"2"}
+              defaultChecked={gameMode}
+              name="mode"
+            />
+          </div>
+          {gameMode === "1" && (
+            <div onChange={handleDifficulty} className="flex justify-evenly">
+              <RadioButton
+                label="Easy"
+                value={"easy"}
+                defaultChecked={difficulty}
+                name="difficulty"
+              />
+              <RadioButton
+                label="Hard"
+                value={"hard"}
+                defaultChecked={difficulty}
+                name="difficulty"
+              />
+            </div>
+          )}
         </div>
+
         <div className="grid grid-cols-3 gap-2 sm:gap-4">
           {squares.map((value, i) => (
             <Square
--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -1,18 +1,19 @@
 interface Props {
   value: string;
   label: string;
-  gameMode: string;
+  defaultChecked: string;
+  name: string;
 }
 
-const RadioButton = ({ value, label, gameMode }: Props) => {
+const RadioButton = ({ value, label, defaultChecked, name }: Props) => {
   return (
     <label className="cursor-pointer flex items-center">
       <input
         type="radio"
-        className="mr-2 sm:mr-3 cursor-pointer text-indigo-400 focus:ring-0"
+        className="mr-2 sm:mr-3 cursor-pointer text-violet-400 focus:ring-0"
         value={value}
-        name="mode"
-        defaultChecked={gameMode === value}
+        name={name}
+        defaultChecked={defaultChecked === value}
       />
       {label}
     </label>
